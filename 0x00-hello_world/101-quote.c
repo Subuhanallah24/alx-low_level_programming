9a76b81e10fbcf0df1aae6f56ff136edd9260144
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-07 16:19:21 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - this prints exactly the input on it
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
