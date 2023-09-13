2f14ca3a3eec3ab4eefd562f44962f07cc91065e
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-13 01:51:27 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet followed by a new line
+ * with a new line
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int num;
+
+
+	for (num = 0; num < 10; num++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	_putchar('\n');
+
+	}
+
+
+
+}
+
