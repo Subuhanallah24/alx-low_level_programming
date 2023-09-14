d0ad67628976bbafbf597aeabc9fdb848cd9ff09
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-14 09:28:59 -0700

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,29 +4,23 @@
  * more_numbers - Print a sequence of numbers multiple times
  *
  * Description:
- * This function prints a sequence of numbers from 0 to 14, followed by a colon,
- * a total of 10 times, each on a new line. It uses the _putchar function to
- * print the numbers as characters.
- *
+ * This function prints a sequence of numbers from 0 to 14
  * Return:
  * This function does not return a value (void).
  */
 
 void more_numbers(void)
 {
-	int colon;
-	int num;
+	int i, j;
 
-	for (colon = 0; colon <= 10; colon++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (num = 0; num < 15; num++)
-		{
-			_putchar(num + '0');
-		}
-
-		_putchar(colon + '0');
+	for (j = 0; j <= 14; j++)
+	{
+	if (j >= 10)
+	_putchar('1');
+	_putchar (j % 10 + '0');
 	}
-
 	_putchar('\n');
-
+	}
 }
