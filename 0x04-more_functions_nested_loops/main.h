1bd6f1d8033556ee658d77696c2b7f5f55de5594
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-14 13:37:31 -0700

    done

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,7 +13,7 @@ int mul(int a, int b);
 void print_line(int n);
 int add(int, int);
 int _isdigit(int c);
-vooid print_diagonal(int n);
+void print_diagonal(int n);
 
 void jack_bauer(void);	
 void print_alphabet(void);
