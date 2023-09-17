5caaeb5bbf9033b7120855ea46da4047c86c3257
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-17 14:10:41 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for printing Fibonacci sequence.
+ *
+ * Description: This program prints the first 50 numbers of the Fibonacci
+ * sequence, starting with 1 and 2. It calculates each subsequent number
+ * by adding the two previous ones.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int n = 50;
+	unsigned long long first = 1, second = 2;
+
+	printf("%llu, %llu", first, second);
+	for (int i = 3; i <= n; i++)
+	{
+		unsigned long long next = first + second;
+
+		printf(", %llu", next);
+
+		first = second;
+		second = next;
+	}
+	printf("\n");
+
+
+	return (0);
+}
