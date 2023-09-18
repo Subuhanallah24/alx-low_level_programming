051f12ed571fd7366b484789d2298aaa25fe452d
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-18 13:35:24 -0700

    2-strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Calculates the length of a string.
+ *
+ * @s: Pointer to the string to be measured.
+ *
+ * Return: The length of the string (number of characters).
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+
+
+}
