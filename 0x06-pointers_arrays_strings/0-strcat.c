c09fdc339e5fbc68f012980403f965fb7798ec90
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-20 09:25:34 -0700

    strcat function inside the string.h header file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Concatenates (appends) two strings.
+ *
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string to be appended.
+ *
+ * Return: Pointer to the destination string after concatenation.
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+
+
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+int _putchar(char c);
+void puts_half(char *str);
+void puts2(char *str);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+void rev_string(char *s);
+void print_rev(char *s);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void _puts(char *str);
+void print_array(int *a, int n);
+void swap_int(int *a, int *b);
+
+#endif
