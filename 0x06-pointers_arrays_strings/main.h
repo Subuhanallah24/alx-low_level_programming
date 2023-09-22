6f8322ef6d44e86f6d1ab298ed611430c690bd07
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-22 13:34:23 -0700

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void reset_to_98(int *n);
 int _putchar(char c);
 char *rot13(char *);
 char *cap_string(char *);
+void print_number(int n);
 char *leet(char *);
 void puts_half(char *str);
 char *string_toupper(char *);
