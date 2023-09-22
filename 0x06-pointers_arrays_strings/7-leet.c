2c0d58f4a908014c7888232a08c910824b75ebf2
Abdullah <abdurahmonabdullah247@gmail.com>
2023-09-22 13:00:25 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,48 +1,25 @@
 #include "main.h"
-
 /**
- * leet - Function that chancges some letters to numbers.
- *
- * @string: Pointer of the string to be changed.
- *
- * Return: Return back the string.
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
-
-
-char *leet(char *string)
+char *leet(char *n)
 {
-	int i;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0; string[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (string[i] == 'a' || string[i] == 'A')
+		for (j = 0; j < 10; j++)
 		{
-			string[i] = '4';
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
 		}
-
-		else if (string[i] == 'e' || string[i] == 'E')
-		{
-			string[i] = '3';
-		}
-
-		else if (string[i] == 'o' || string[i] == 'O')
-		{
-			string[i] = '0';
-		}
-
-		else if (string[i] == 't' || string[i] == 'T')
-		{
-			string[i] = '7';
-		}
-
-		else if (string[i] == 'l' || string[i] == 'L')
-		{
-			string[i] = '1';
-		}
-
-
 	}
-
-	return (string);
-
+	return (n);
 }
+
