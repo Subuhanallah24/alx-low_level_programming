a732fb63f499cf5db42970303929762b74fed4c7
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-03 17:09:16 -0700

    3rd Augument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main  - It prints the multiplication of some input arguments
+ *
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 1 if argc is equal to 1 and 0 otherwise.
+ */
+
+
+int main(int argc, char **argv)
+{
+	int mul1, mul2;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	mul1 = atoi(argv[1]);
+	mul2 = atoi(argv[2]);
+
+	printf("%d\n", mul1 * mul2);
+
+	return (0);
+}
