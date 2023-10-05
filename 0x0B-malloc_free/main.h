5df3ffca658df9ef219f33e04862abc4009a4690
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-05 15:50:59 -0700

    101 -strtow

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 void _putchar(char c);
+char **strtow(char *str);
 int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
 void free_grid(int **grid, int height);
