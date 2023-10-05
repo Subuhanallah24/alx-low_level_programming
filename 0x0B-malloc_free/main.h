190bcae7724420cb5a31b5b6ceb9bc99e2eb85f1
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-05 15:40:39 -0700

    100 - argstostr

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,7 @@
 
 void _putchar(char c);
 int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
 void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
