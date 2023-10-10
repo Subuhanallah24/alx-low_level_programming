0ad5d7251972e5735d1bb74a738019e0b76a21f9
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-10 14:04:42 -0700

    dog header file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a readme file for structure and typedef. In which structure are used to store related data to form information, and typedef is used to change the name of a data type.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- dog structure
+ *
+ * @name: pointer to a char type
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: A header file for the dog sructure.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
