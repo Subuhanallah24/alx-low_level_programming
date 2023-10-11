ad17db71901aae0d3c3c72991655c67da7bf0541
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-11 14:09:34 -0700

    print filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the filename of the source file.
+ *
+ * Return: Always 0 (success).
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
