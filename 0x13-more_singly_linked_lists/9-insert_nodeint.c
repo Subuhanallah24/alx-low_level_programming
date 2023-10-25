ec6c8dbbbe4235673ddc086d0fa592e4d19a0e40
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-25 16:30:06 -0700

    add node at the specify index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode;
 
 	unsigned int track_index = 0;
+	
+	if (!ptr)
+	{
+		newNode = malloc(sizeof(listint_t));
+
+		if (!newNode)
+			return (NULL);
+
+		newNode->next = NULL;
+		newNode->n = n;
+
+		*head = newNode;
+
+		return (newNode);
+	}
 
 	while (ptr)
 	{
