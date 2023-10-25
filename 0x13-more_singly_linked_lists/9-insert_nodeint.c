488fe5c203eaecba7ad8e95a13658cdc3945ce51
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-25 16:48:23 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,33 +12,25 @@
  * returns the address of the newNode.
  */
 
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr = *head;
 	listint_t *newNode;
+	unsigned int track_index = -1;
 
-	unsigned int track_index = 0;
-	
-	if (!ptr)
+	if (idx == 0)
 	{
 		newNode = malloc(sizeof(listint_t));
-
 		if (!newNode)
 			return (NULL);
-
 		newNode->next = NULL;
 		newNode->n = n;
-
 		*head = newNode;
-
 		return (newNode);
 	}
-
 	while (ptr)
 	{
 		track_index++;
-
 		if (track_index == idx)
 		{
 			newNode = malloc(sizeof(listint_t));
@@ -46,16 +38,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			/*if newNode is NULL*/
 			if (!newNode)
 				return (NULL);
-
 			newNode->next = (ptr)->next;
 			newNode->n = n;
 
 			(ptr)->next = newNode;
 			return (newNode);
 		}
-
 		ptr = ptr->next;
 	}
-
 	return (NULL);
 }
