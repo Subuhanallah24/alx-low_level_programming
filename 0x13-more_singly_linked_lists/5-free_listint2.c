19fbcf585bc83241f714c5273f2b5412e43ca99f
Abdullah <abdurahmonabdullah247@gmail.com>
2023-10-25 05:53:56 -0700

    freed 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2- free list
+ * @head: The head.
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		(*head) = (*head)->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+
+/**
+ * pop_listint- delete the head node.
+ *
+ * @head: The head.
+ *
+ * Return: the value of the deleted node.
+ */
+
+
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp = *head;
+	int value = *head->n;
+
+	if (*head == NULL)
+		return (0);
+
+	*head = *head->next;
+	free(tmp);
+
+	return (value);
+
+}
